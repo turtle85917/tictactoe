229eff5a132ae05537728868b8233a23871c43b5
turtle85917 <siu85917@gmail.com>
2023-01-24 10:59:57 +0900

    Defines input rules

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,46 @@
 import * as readline from "node:readline";
+
 const stdin = process.openStdin();
+const board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']];
 
-clearScreen();
-process.stdout.write("Input : ");
 process.stdin.setEncoding("utf-8");
+printBoard();
+write("\nLocation : ");
 
-stdin.addListener("data", function(d) {
+stdin.addListener("data", (data) => {
   clearScreen();
-  if (String(d).trim() === "/kill") process.exit();
-  process.stdout.write(`You entered: ${String(d).trim()}\nInput : `);
+  const input = String(data).trim();
+  printBoard();
+  if (input === "quit") process.exit(10);
+  if (!/\(\d,( |)\d\)/.test(input)) {
+    locationWrite("\nInvaild location. e.g) (1, 2)");
+    return;
+  }
+  const position = input.slice(1, -1).split(',').map(Number);
+  if (position[0] < 1 || position[0] > 3 || position[1] < 1 || position[1] > 3) {
+    locationWrite("\nInvaild location. e.g) (1, 2)");
+    return;
+  }
+  locationWrite();
 });
 
 process.on("exit", () => {
-  process.stdout.write("Kill process.");
+  console.log("\nKill process.");
 });
+
+function printBoard() {
+  clearScreen();
+  write(board.map(tiles => tiles.map(tile => ` ${tile} `).join('|')).join('\n-----------\n'));
+}
+
+function locationWrite(content: string = '') {
+  write(content);
+  write("\nLocation : ");
+}
+
+function write(content: string) {
+  process.stdout.write(content);
+}
 
 function clearScreen() {
   console.log('\n'.repeat(Math.max(process.stdout.rows - 2, 0)));
